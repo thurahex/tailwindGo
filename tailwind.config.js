4d8c0e71293e379d3fbd601b160799da47f74732
thurahex <thura.workmail@gmail.com>
2023-09-01 10:34:18 +0630

    added the dark mode to the card html, then created nav.html empty file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/*.{html,js}"],
+  darkMode:"class",
   theme: {
-    colors:{
-      primary:{
-        
-          100: "#e2f6ff",
-          200: "#c4eeff",
-          300: "#a7e5ff",
-          400: "#89ddff",
-          500: "#6cd4ff",
-          600: "#56aacc",
-          700: "#417f99",
-          800: "#2b5566",
-          900: "#162a33",
-      }
-    },
+    
     fontFamily:{
       'poppin':['Poppins','san'],
     },
